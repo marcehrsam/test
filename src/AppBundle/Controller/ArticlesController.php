d011438de9aa389269449a27ce66a992c6352cdb
Marc Ehrsam <marc_ehrsam@web.de>
2018-03-10 22:03:31 +0100

    bibliotheken hinzugefügt
    bootstrap
    jquery
    main.css
    narrow-jumbotron

---
+++ src/AppBundle/Controller/ArticlesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ArticlesController extends Controller
+{
+    /**
+    * @Route("/page1", name="page1")
+    * @return Response
+    */
+   public function page1Action()
+   {
+       return $this->render('@App/Articles/page1.html.twig',
+           array(
+               // ...
+           )
+       );
+    }
+    
+    /**
+     * @Route("/page2", name="page2")
+     * @return Response
+     */
+    public function page2Action()
+    {
+        return $this->render('@App/Articles/page2.html.twig',
+            array(
+                // ...
+            )
+        );
+    }
+}
