c1f651aff43a799f1abf6019af44294ff36e0862
Marc Ehrsam <marc_ehrsam@web.de>
2018-03-13 20:42:05 +0100

    Formular eingebaut
    .

--- src/TarifrechnerBundle/Controller/MyController.php
+++ src/TarifrechnerBundle/Controller/MyController.php
@@ -4,16 +4,45 @@ namespace TarifrechnerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MyController extends Controller
 {
     /**
+     * @Route("/anzeige", name="anzeige")
+     * @return Response
+     */
+    public function anzeigeAction()
+    {
+        return $this->render('@Tarifrechner/My/anzeige.html.twig');
+    }
+    
+    /**
      * @Route("/page3", name="p3")
      * @return Response
      */
-    public function indexAction()
-    {
-        return $this->render('@Tarifrechner/My/index.html.twig');
+    public function newFormAction(Request $request){
+        
+        //Formular bauen
+        $form = $this->createFormBuilder()
+                ->add('bezeichnung', TextType::class)
+                ->add('save', SubmitType::class, array('label' => 'Speichern'))
+                ->getForm();
+        
+        //Eventhandling (Submit)
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            return $this->redirectToRoute('anzeige');
+        }
+        
+        //createView immer nach handleRequest!
+        return $this->render('@Tarifrechner/My/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
 }
